37c1b9117d1daee5e0d4be32b962ed6c4b572ff9
carlo-vegesack <carlo.vegesack@gmail.com>
2023-09-08 10:41:35 +0200

    corrected

--- src/print-1-10.py
+++ src/print-1-10.py
@@ -1,6 +1,6 @@
 
 # Print the numbers described in the exercise
 a = 1
-while a < 10:
+while a < 11:
     print(a)
     a += 1
---
+++ src/test.py
@@ -0,0 +1,8 @@
+table= {}
+string = 'larloll'
+i = 0
+for c in string:
+    print(c)
+    if c == 'c' or 'C':
+        i += 1
+print(i)
