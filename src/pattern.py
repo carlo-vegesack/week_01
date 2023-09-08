b2f769b3bb81e8c2eec36a4f20e5fd48c5d3f6b1
carlo-vegesack <carlo.vegesack@gmail.com>
2023-09-08 09:03:32 +0200

    pattern_final

--- src/pattern.py
+++ src/pattern.py
@@ -1,10 +1,10 @@
 # Print the pattern
 for x in range(1,10):
     if x < 6:
-        print(*x*('*',), sep='')
+        print(*x*('*',), sep=' ')
     
 for y in range(4,0,-1):
-    print(*y*('*',), sep='')
+    print(*y*('*',), sep=' ')
 
 
 
